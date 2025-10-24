61b06cfee39def553e875622fb7a35078d960c81
nurbolot2006 <kemelbekovnurbolot201@gmail.com>
2025-10-24 21:21:26 +0600

    save

--- src/pages/landing/Home/Home.jsx
+++ src/pages/landing/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Card from './ui/card/Card';
+import Courses from "./ui/courses/Courses.jsx";
 
 function Home() {
-  return (
-    <div>
-      <Card/>
-    </div>
-  );
+    return (
+        <div>
+            <Card/>
+            <Courses/>
+        </div>
+    );
 }
 
 export default Home;
