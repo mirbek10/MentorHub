26b3bf7bed9e6e2a4736c770a5ae58ffa600323a
mirbek10 <mirbekbook@gmail.com>
2025-10-24 21:18:54 +0600

    added banner

--- src/pages/landing/Home/Home.jsx
+++ src/pages/landing/Home/Home.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Card from './ui/card/Card';
-import Hero2 from './ui/hero2/Hero2';
+import React from "react";
+import Card from "./ui/card/Card";
+import Hero2 from "./ui/hero2/Hero2";
+import Banner from "./ui/banner/Banner";
 
 function Home() {
   return (
     <div>
-      <Card/>
-      <Hero2/>
+      <Banner />
+      <Card />
+      <Hero2 />
     </div>
   );
 }
